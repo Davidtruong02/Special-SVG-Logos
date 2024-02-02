b77ba3a3e7808674f180e06d83a69bff0fd3a772
David Truong <Davidtruong134@gmail.com>
2024-02-02 11:45:16 -0600

    Added shape code w/ extensions and SVG file

---
+++ Lib/shape.js
@@ -0,0 +1,34 @@
+
+// Shape class is the base class for all shapes
+class Shape {
+    constructor(){
+        this.color = "";
+    }
+    setColor(color){
+        this.color = color;
+    }
+}
+
+
+// Circle class is a subclass of Shape
+class Circle extends Shape {
+    render(){
+        return `<circle cx="100" cy="100" r="50" fill="${this.color}" />`;
+    }
+}
+
+// Rectangle class is a subclass of Shape
+class Rectangle extends Shape {
+    render(){
+        return `<rect x="50" y="50" width="100" height="100" fill="${this.color}" />`;
+    }
+}
+// Triangle class is a subclass of Shape
+class Triangle extends Shape {
+    render(){
+        return `<polygon points="100,50 50,100 150,100" fill="${this.color}" />`;
+    }
+}
+
+// Export the classes
+module.exports = { Circle, Rectangle, Triangle };
---
+++ Lib/svg.js
@@ -0,0 +1,21 @@
+class SVG {
+    constructor(){
+        this.shape = "";
+        this.text = "";
+    }
+
+    render(){
+        return `svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        ${this.shape} ${this.text} </svg>`;
+}
+
+    setText(userText, userColor){
+        this.text = `<text x="150" y="125" font-size="20" text-anchor="middle" fill="${userColor}">${userText}</text>`;
+}
+
+
+    setShape(userShape){
+        this.shape = userShape.render();
+    }
+}
+module.exports = SVG;
